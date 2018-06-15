65ca0d4e3342d8d5833e17ca4e2a5dce4bdbc6f7
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-06-15 17:18:21 -0300

    Exemplo-Construtor

---
+++ ExemploConstrutor/WFA/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantidadeGolsFeitos = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFaturamento = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtCadastroRapidoPais = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCadastroRapidoNome = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCadastroRapido = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(53, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(242, 22);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtPais
+            // 
+            this.txtPais.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPais.Location = new System.Drawing.Point(53, 122);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(242, 22);
+            this.txtPais.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "País";
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSerie.Location = new System.Drawing.Point(53, 179);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(242, 22);
+            this.txtSerie.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Série";
+            // 
+            // txtQuantidadeGolsFeitos
+            // 
+            this.txtQuantidadeGolsFeitos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantidadeGolsFeitos.Location = new System.Drawing.Point(53, 236);
+            this.txtQuantidadeGolsFeitos.Name = "txtQuantidadeGolsFeitos";
+            this.txtQuantidadeGolsFeitos.Size = new System.Drawing.Size(242, 22);
+            this.txtQuantidadeGolsFeitos.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 20);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Quantidade de Gols Feitos";
+            // 
+            // txtFaturamento
+            // 
+            this.txtFaturamento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFaturamento.Location = new System.Drawing.Point(597, 65);
+            this.txtFaturamento.Name = "txtFaturamento";
+            this.txtFaturamento.Size = new System.Drawing.Size(242, 22);
+            this.txtFaturamento.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(593, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 20);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Faturamento";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(674, 179);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(165, 45);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtCadastroRapidoPais
+            // 
+            this.txtCadastroRapidoPais.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCadastroRapidoPais.Location = new System.Drawing.Point(597, 368);
+            this.txtCadastroRapidoPais.Name = "txtCadastroRapidoPais";
+            this.txtCadastroRapidoPais.Size = new System.Drawing.Size(242, 22);
+            this.txtCadastroRapidoPais.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(593, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "País";
+            // 
+            // txtCadastroRapidoNome
+            // 
+            this.txtCadastroRapidoNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCadastroRapidoNome.Location = new System.Drawing.Point(53, 368);
+            this.txtCadastroRapidoNome.Name = "txtCadastroRapidoNome";
+            this.txtCadastroRapidoNome.Size = new System.Drawing.Size(242, 22);
+            this.txtCadastroRapidoNome.TabIndex = 28;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(49, 345);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 27;
+            this.label7.Text = "Nome";
+            // 
+            // btnCadastroRapido
+            // 
+            this.btnCadastroRapido.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastroRapido.Location = new System.Drawing.Point(586, 485);
+            this.btnCadastroRapido.Name = "btnCadastroRapido";
+            this.btnCadastroRapido.Size = new System.Drawing.Size(253, 45);
+            this.btnCadastroRapido.TabIndex = 31;
+            this.btnCadastroRapido.Text = "Cadastro Rapido";
+            this.btnCadastroRapido.UseVisualStyleBackColor = true;
+            this.btnCadastroRapido.Click += new System.EventHandler(this.btnCadastroRapido_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 559);
+            this.Controls.Add(this.btnCadastroRapido);
+            this.Controls.Add(this.txtCadastroRapidoPais);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCadastroRapidoNome);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtFaturamento);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtQuantidadeGolsFeitos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtQuantidadeGolsFeitos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFaturamento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox txtCadastroRapidoPais;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCadastroRapidoNome;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCadastroRapido;
+    }
+}
+
